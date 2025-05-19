3fcb07f9cc6a9e42b92116965672f68102570105
andegaven <mariaostapenko453@gmail.com>
2025-05-19 11:56:48 +0300

    third commit

--- ship.py
+++ ship.py
@@ -3,7 +3,7 @@ class Ship():
     def __init__(self, ai_settings, screen):
         self.screen = screen
         self.ai_settings = ai_settings
-        self.image = pygame.image.load('cat.png')
+        self.image = pygame.image.load('venv/images/cat.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
@@ -11,6 +11,10 @@ class Ship():
         self.center = float(self.rect.centerx)
         self.moving_right = False
         self.moving_left = False
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
     def update(self):
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
@@ -19,3 +23,5 @@ class Ship():
         self.rect.centerx = self.center
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+    def center_ship(self):
+        self.center = self.screen_rect.centerx
